7f259b6fe89ad1d76c2861d59c295b861aea7b07
Juan Sebastián Álvarez Higuita - Ceiba Software <sebastian.alvarez@ceiba.com.co>
2021-08-11 08:24:14 -0500

    test navbar

---
+++ src/app/components/Navbar/Navbar.test.tsx
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom/extend-expect';
+import Navbar from './Navbar';
+import {render} from '@testing-library/react'
+test('render navbar', () => {
+
+    expect(true).toBeTruthy()
+
+
+
+
+
+
+    
+})
