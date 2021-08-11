30b5ea196d2186b0397807a9947dcd86b66a5c05
Juan Sebastián Álvarez Higuita - Ceiba Software <sebastian.alvarez@ceiba.com.co>
2021-08-10 22:36:54 -0500

    changes in test

--- src/app/components/FormReservation/FormReservation.test.tsx
+++ src/app/components/FormReservation/FormReservation.test.tsx
@@ -4,12 +4,6 @@ import {render} from '@testing-library/react'
 import FormReservation from './FormReservation';
 import {prettyDOM} from '@testing-library/dom'
 
-/* describe('Form element tests', () => {
-
-    let container:HTMLElement
-
-    it('Render Select in form', () => {
-        const input = container.querySelector('Input')
-        expect(input).toHaveLength(5)
-    })
-}) */
+test('TravelCo Form', () => {
+    expect(true).toBeTruthy()
+})
