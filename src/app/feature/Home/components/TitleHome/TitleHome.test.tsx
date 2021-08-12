b147e6a333236c6bcea5062a6bd0c1900b65b706
Juan Sebastián Álvarez Higuita - Ceiba Software <sebastian.alvarez@ceiba.com.co>
2021-08-12 10:30:21 -0500

    sonar-properties

--- src/app/feature/Home/components/TitleHome/TitleHome.test.tsx
+++ src/app/feature/Home/components/TitleHome/TitleHome.test.tsx
@@ -1,37 +1,8 @@
-import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import {render, cleanup} from '@testing-library/react'
-import TitleHome from './TitleHome';
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-
-afterEach(cleanup)
-
-const startingState = {}
 
 interface Action {
     type:string
 }
-
-function reducer (state = startingState, action : Action) {
-    switch (action.type) {
-        default:
-            return state;
-
-    }
-}
-
-
-
-function renderWithRedux (component:any, {store = createStore(reducer, startingState)} = {}) {
-    return {
-        ...render(<Provider store={store}>{component}</Provider> )
-    }
-} 
-
-test('TitleHome Render', () => {
-    renderWithRedux( <TitleHome /> )
-}) 
 
 
 
