42d0d5d4aa512dbb6aa2ea90084e5ad8cc3359bd
Juan Sebastián Álvarez Higuita - Ceiba Software <sebastian.alvarez@ceiba.com.co>
2021-08-12 10:58:07 -0500

    changes

--- src/app/feature/Home/components/TitleHome/TitleHome.test.tsx
+++ src/app/feature/Home/components/TitleHome/TitleHome.test.tsx
@@ -1,8 +1,20 @@
-import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import TitleHome from './TitleHome';
+import { mount } from 'enzyme';
 
 interface Action {
     type:string
 }
+
+describe('Pruebas al componente TitleHome', () => {
+
+    const Title = renderer.create(<TitleHome />).toJSON();
+
+    test('Snapshot Component', () => {
+        expect(Title).toMatchSnapshot();
+    });
+})
 
 
 
